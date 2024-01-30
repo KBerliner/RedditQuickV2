a3f030786180b0431ab22b5cf9c504a5076825cd
Kyle <berlinerkyle@gmail.com>
2024-01-30 10:48:18 -0700

    Added some more styling again. Next is the search feature.

--- src/components/PostGrid/PostGrid.jsx
+++ src/components/PostGrid/PostGrid.jsx
@@ -9,7 +9,7 @@ export default function PostGrid({ posts }) {
     return (
         <>
             <ul className={styles.grid_container}>
-                {posts.length > 0 ? posts.map(post => <PostThumbnail key={post.id} preview={post.preview} title={post.title} text={post.selftext} />) : <h1>There are no posts to be displayed right now.</h1>}
+                {posts.length > 0 ? posts.map(post => <PostThumbnail key={post.id} subreddit={post.subreddit_name_prefixed} preview={post.preview} title={post.title} text={post.selftext} />) : <h1>There are no posts to be displayed right now.</h1>}
             </ul>
         </>
     )
--- src/components/PostThumbnail/PostThumbnail.jsx
+++ src/components/PostThumbnail/PostThumbnail.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './PostThumbnail.module.css';
 
-export default function PostThumbnail({ title, preview }) {
-    console.log(preview);
+export default function PostThumbnail({ title, text, preview, subreddit_name_prefixed }) {
 
     return (
         <>
             <li className={styles.thumbnail_container}>
                 <h5>{title}</h5>
-                {preview ? <img src={preview.images[0].source.url} /> : <></>}
+                {preview ? <img src={preview.images[0].source.url} /> : <p>{text}</p>}
             </li>
         </>
     )
